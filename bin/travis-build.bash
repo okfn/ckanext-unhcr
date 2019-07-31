d5926ff84548305d0cfadee3a1855d4ea0aba05c
amercader <amercadero@gmail.com>
2019-07-31 12:56:52 +0200

    Fix #197. Update travis image, solr setup and remove psycopg2 hack

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -25,9 +25,6 @@ for d in $TRAVIS_BUILD_DIR/bin/patches/*; do \
     done ; \
 done
 
-# Unpin CKAN's psycopg2 dependency get an important bugfix
-# https://stackoverflow.com/questions/47044854/error-installing-psycopg2-2-6-2
-sed -i '/psycopg2/c\psycopg2' requirements.txt
 python setup.py develop
 pip install -r requirements.txt
 pip install -r dev-requirements.txt
@@ -39,9 +36,13 @@ sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
 sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
 
-echo "SOLR config..."
+
+echo "Setting up Solr..."
 # Solr is multicore for tests on ckan master, but it's easier to run tests on
 # Travis single-core. See https://github.com/ckan/ckan/issues/2972
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
+sudo service jetty restart
 sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
 
 echo "Initialising the database..."
