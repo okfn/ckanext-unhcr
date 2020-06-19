b31cf719b707b1eb2758bfa0690bc4c662344350
amercader <amercadero@gmail.com>
2020-06-20 00:06:13 +0200

    Fix wrong var

--- ckanext/unhcr/validators.py
+++ ckanext/unhcr/validators.py
@@ -181,7 +181,7 @@ def file_type_validator(key, data, errors, context):
 
 def upload_not_empty(key, data, errors, context):
     index = key[1]
-    id_key = ('resources', key, 'id')
+    id_key = ('resources', index, 'id')
 
     is_create = not data.get(id_key) or data.get(id_key) is missing
     is_update = not is_create
