1e68478b2dc5fe4313c04cf3409ef1e9b0f17834
chris48s <chris.shaw480@gmail.com>
2021-03-24 13:25:08 +0000

    pin all extensions installed under test

--- bin/install-extensions.bash
+++ bin/install-extensions.bash
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 
-git clone https://github.com/ckan/ckanext-scheming
+git clone --depth 1 --branch release-2.1.0 https://github.com/ckan/ckanext-scheming
 (cd ckanext-scheming && python setup.py develop)
 
-git clone https://github.com/okfn/ckanext-hierarchy
+git clone --depth 1 --branch v0.2.2 https://github.com/okfn/ckanext-hierarchy
 (cd ckanext-hierarchy && python setup.py develop)
 
-git clone https://github.com/okfn/ckanext-collaborators
+git clone --depth 1 --branch 0.0.9 https://github.com/okfn/ckanext-collaborators
 (cd ckanext-collaborators && python setup.py develop)
 
-git clone https://github.com/keitaroinc/ckanext-s3filestore
-(cd ckanext-s3filestore && git checkout ckan-2.8 && python setup.py develop && pip install -r requirements.txt)
+git clone --depth 1 --branch ckan-2.8 https://github.com/keitaroinc/ckanext-s3filestore
+(cd ckanext-s3filestore && python setup.py develop && pip install -r requirements.txt)
